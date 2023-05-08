5f94843cefe795cc92223c635d50f297bed4b71d
darhan1168 <darinahanina04@gmail.com>
2023-05-08 09:14:18 +0200

    Create class Task

---
+++ MainProject/Core/Models/Task.cs
@@ -0,0 +1,13 @@
+using Core.Enums;
+
+namespace Core;
+
+public class Task : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public TaskPriority TaskPriority { get; set; }
+    public User User { get; set; }
+    public TaskProgress TaskProgress { get; set; } 
+}
