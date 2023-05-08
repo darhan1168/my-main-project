9fa97529681e174a7bad38a4555ae1005d4d9ec1
darhan1168 <darinahanina04@gmail.com>
2023-05-08 09:12:55 +0200

    Create class User

---
+++ MainProject/Core/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Core;
+
+public class User : BaseEntity
+{ 
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
