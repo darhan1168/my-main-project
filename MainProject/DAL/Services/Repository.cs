3758d0b4c8de31e6f147b02397f0280d72b3e34f
darhan1168 <darinahanina04@gmail.com>
2023-05-09 10:59:29 +0200

    Inherit interface IRepository
    without implementation

---
+++ MainProject/DAL/Services/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+using DAL.Abstraction.Interfaces;
+
+namespace DAL
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        public List<T> GetAll(int pageNumber = 1, int pageSize = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByPredicate(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, T updateObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
