9f6b10e44e5061dd37972fb33d4809f47ff9035c
darhan1168 <darinahanina04@gmail.com>
2023-05-25 08:41:34 +0200

    Update Program.cs

--- MainProject/UI/Program.cs
+++ MainProject/UI/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace UI
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace UI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            var serviceProvider = DependencyRegistration.Register();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var appManager = scope.ServiceProvider.GetService<AppManager>();
+                appManager.Start();
+            }
         }
     }
 }
