2727dd340a2d527888ae9c11ac2d818c6bd7d817
darhan1168 <darinahanina04@gmail.com>
2023-05-25 08:41:30 +0200

    Create DependencyRegistration.cs

---
+++ MainProject/UI/DependencyRegistration.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using UI.ConsoleManagers;
+using UI.Interfaces;
+
+namespace UI;
+
+public class DependencyRegistration
+{
+    public static IServiceProvider Register()
+    {
+        var services = new ServiceCollection();
+        services.AddScoped<AppManager>();
+        services.AddScoped<UserConsoleManager>();
+        services.AddScoped<TaskConsoleManager>();
+        services.AddScoped<ProjectConsoleManager>();
+        
+        foreach (var type in typeof(IConsoleManager<>).Assembly.GetTypes()
+                     .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces()
+                         .Any(i=> i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IConsoleManager<>))))
+        {
+            var interfaceType = type.GetInterfaces().First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IConsoleManager<>));
+            services.AddScoped(interfaceType, type);
+        }
+        
+        BLL.DependencyRegistration.RegisterServices(services);
+        
+        return services.BuildServiceProvider();
+    }
+}
