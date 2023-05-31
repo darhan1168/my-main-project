ffdd37278c3578e383e919530d95c5e7feb8f5fa
darhan1168 <darinahanina04@gmail.com>
2023-05-31 13:45:05 +0200

    Update ProjectConsoleManager.cs

--- MainProject/UI/ConsoleManagers/ProjectConsoleManager.cs
+++ MainProject/UI/ConsoleManagers/ProjectConsoleManager.cs
@@ -91,7 +91,31 @@ public class ProjectConsoleManager : ConsoleManager<IProjectService, Project>, I
     {
         try
         {
+            if (!_user.Role.Equals(UserRole.Stakeholder))
+            {
+                Console.WriteLine("Only stakeholder can create project");
+                return;
+            }
             
+            Console.Clear();
+            Console.Write("Enter title:");
+            string title = Console.ReadLine();
+            
+            Console.Write("Enter description:");
+            string description = Console.ReadLine();
+
+            var users = GetUsers();
+            var tasks = GetTasks();
+            
+            _service.CreateProject(new Project()
+            {
+                Id = Guid.NewGuid(),
+                Title = title,
+                Description = description,
+                Users = users,
+                Tasks = tasks
+            });
+            Console.WriteLine("Your project successfully added");
         }
         catch (Exception ex)
         {
@@ -103,7 +127,19 @@ public class ProjectConsoleManager : ConsoleManager<IProjectService, Project>, I
     {
         try
         {
+            if (!_user.Role.Equals(UserRole.Stakeholder))
+            {
+                Console.WriteLine("Only stakeholder can delete project");
+                return;
+            }
+
+            var projects = GetAllProjects();
+            DisplayAllProjects();
             
+            Console.WriteLine("Enter number of project for deleting");
+            int input = Int32.Parse(Console.ReadLine());
+            
+            _service.DeleteProject(projects[input - 1].Id);
         }
         catch (Exception ex)
         {
@@ -168,8 +204,64 @@ public class ProjectConsoleManager : ConsoleManager<IProjectService, Project>, I
             index++;
         }
     }
+
+    private List<User> GetUsers()
+    {
+        List<User> users = new List<User>();
+        users.Add(_user);
+        while (true)
+        {
+            Console.WriteLine("Enter username, which need to add");
+            string username = Console.ReadLine();
+            User user = _userConsoleManager.GetByPredicate(u => u.Username.Equals(username));
+            users.Add(user);
+            Console.WriteLine($"Username: {user.Username}, Role: {user.Role} - will add in project");
+            
+            Console.WriteLine("Enter 1 if you want to add more");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                continue;
+            }
+
+            return users;
+        }
+    }
+
+    private List<Task> GetTasks()
+    {
+        List<Task> tasks = new List<Task>();
+        while (true)
+        {
+            var task = _taskConsoleManager.GetTask("add in project");
+            if (tasks.Contains(task))
+            {
+                Console.WriteLine("This task already added");
+                continue;
+            }
+            
+            tasks.Add(task);
+            Console.WriteLine($"Your tasks will add in project");
+            
+            Console.WriteLine("Enter 1 if you want to add more");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                continue;
+            }
+
+            return tasks;
+        }
+    }
+ 
     public void GetUser(Guid userId)
     {
         _user = _userConsoleManager.GetById(userId);
+    }
+
+    public void Task()
+    {
+        _taskConsoleManager.GetUser(_userConsoleManager.User.Id);
+        _taskConsoleManager.PerformOperations();
     }
 }
