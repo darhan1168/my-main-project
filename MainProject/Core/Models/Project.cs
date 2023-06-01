5cead599cd602cf7c8e333929ba8aa5cdd17d0ae
darhan1168 <darinahanina04@gmail.com>
2023-06-01 16:30:27 +0200

    Add CompletionRate

--- MainProject/Core/Models/Project.cs
+++ MainProject/Core/Models/Project.cs
@@ -4,6 +4,7 @@ public class Project : BaseEntity
 {
     public string Title { get; set; }
     public string Description { get; set; }
+    public double CompletionRate { get; set; }
     public List<User> Users { get; set; }
     public List<Task> Tasks { get; set; }
 }
